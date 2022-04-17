fae4e0b53a0e2284e380a53b045145da950cc865
jjohnson0204 <joejohnson0204@gmail.com>
2022-04-16 21:18:02 -0400

    Hacker Rank string file: - JavaSubstring.java

---
+++ HackerRank/Java/Introduction/src/String/JavaSubstring.java
@@ -0,0 +1,19 @@
+package String;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class JavaSubstring {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        // solution:
+        in.close();
+        System.out.println(S.substring(start, end));
+    }
+}
